18341d5ed281fac928509b1be9cdcffc0b8f8699
Xm3Ga <adripbahamont@gmail.com>
2023-06-09 17:35:48 +0200

    changes a lot of things (what the hell i'm doing)

---
+++ ejercicios/entregas/adrianPérez/proyectoFinalProgra/src/main/Snail.java
@@ -0,0 +1,26 @@
+package main;
+
+public class Snail {
+    private double profundidadAgua = 20;
+    private double profundidadCaracol = 0;
+    private int dia = 0;
+
+    public int getDia() {
+        return dia;
+    }
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+    public double getProfundidadCaracol() {
+        return profundidadCaracol;
+    }
+    public void setProfundidadCaracol(double profundidadCaracol) {
+        this.profundidadCaracol = profundidadCaracol;
+    }
+    public double getProfundidadAgua() {
+        return profundidadAgua;
+    }
+    public void setProfundidadAgua(double profundidadAgua) {
+        this.profundidadAgua = profundidadAgua;
+    }
+}
