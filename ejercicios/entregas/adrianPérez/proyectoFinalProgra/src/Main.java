c41657dc1ad46aeadffdd1429297c04183fd8e1d
Xm3Ga <adripbahamont@gmail.com>
2023-06-09 12:00:19 +0200

    hola

--- ejercicios/entregas/adrianPérez/proyectoFinalProgra/src/Main.java
+++ ejercicios/entregas/adrianPérez/proyectoFinalProgra/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Este es el inicio del proyecto.");
+        System.out.println("Este es el inicio del proyecto.holatest");
     }
 }
