f7b0e4490d79ad200b09e43a03ac7758b45afb4d
Jose Salceda <pepesc2004@gmail.com>
2023-06-13 19:51:22 +0200

    Minor minor minor fix to rain

--- ejercicios/entregas/adrianPérez/proyectoFinalProgra/src/main/WeatherConditionWorse.java
+++ ejercicios/entregas/adrianPérez/proyectoFinalProgra/src/main/WeatherConditionWorse.java
@@ -5,7 +5,7 @@ public class WeatherConditionWorse implements WeatherCondition{
     Snail snail = new Snail();
 
     public void subidaAguaLLuvia(){
-        snail.setProfundidadAgua(snail.getProfundidadAgua() - 2);
+        snail.setProfundidadAgua(snail.getProfundidadAgua() - 5);
         System.out.println("Lueve fuertemente, el agua sube 5 metros");
     }
 }
